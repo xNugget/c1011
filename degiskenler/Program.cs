d12c540c34796d61dd7a2f894886e03ac7b6e90a
xNugget <llberkayyilmazll@hotmail.com>
2022-03-21 14:39:43 +0300

    updta
    aa

---
+++ degiskenler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace degiskenlerr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int denemesayı = 15;
+           string leblebi = null;
+           string Leblebi = null;
+           bool ısık = false;
+           bool kapı = true ;
+            Console.WriteLine(denemesayı);
+string gunler = DateTime.Now.ToString("dd.MM.yyyy");
+Console.WriteLine(gunler);
+int uuu = 30;
+string uuu2 = "20";
+int ular = uuu + Convert.ToInt32(uuu2);
+string ular2 = uuu2 + uuu.ToString();
+Console.WriteLine(ular + " " + ular2);
+
+
+
+        
+        }
+    }
+}
---
+++ konsol/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace konsol
+{
+class program
+{
+
+public static void Main(string[] args)
+{
+Console.WriteLine("helloo");
+Console.WriteLine("ismini bagısla:");
+string isimne = Console.ReadLine();
+Console.WriteLine("soyisimnedir");
+string soyisimne = Console.ReadLine();
+Console.WriteLine("hosgeldin" + isimne + soyisimne);
+
+
+
+
+
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
+}
