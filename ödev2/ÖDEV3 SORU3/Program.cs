dd47c13130d49fe8194f98439597647984633d48
xNugget <llberkayyilmazll@hotmail.com>
2022-04-03 17:43:39 +0300

    ödev3

---
+++ ödev2/ÖDEV3 SORU3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ Console.WriteLine("Enter a sentence : ");
+            ArrayList vowelList = new ArrayList();
+            char[] vowels = { 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U','ö'};
+            string letters = Console.ReadLine();
+            for (int i = 0; i < Convert.ToInt32(letters.Length); i++)
+            {
+                for (int j = 0; j < Convert.ToInt32(vowels.Length); j++)
+                {
+                    if (letters[i].Equals(vowels[j]))
+                    {
+                        vowelList.Add(letters[i]);
+                        Console.WriteLine(letters[i]);
+                    }
+                }
+            }
+            
