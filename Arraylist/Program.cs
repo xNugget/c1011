5da55c1bb79f8ec9122b8b900adbf8de65ea076c
xNugget <llberkayyilmazll@hotmail.com>
2022-04-03 14:11:24 +0300

    arraylist

--- Arraylist/Program.cs
+++ Arraylist/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList liste = new ArrayList();
+            // liste. Add("Ayşe");
+            // liste.Add(2);
+            // liste.Add(true);
+            // liste. Add('A');
+            //içerisinde verilere erişim    
+    //                     Console.WriteLine(liste[1]);
+    //             foreach (var item in liste)
+    // Console.WriteLine(item);
+//AddRange
+Console.WriteLine("***** Add Range ******");
+// string [] renkler = {"kırmızı","sarı","yesil"};
+List<int> sayılar = new List<int>(){1,8,3,7,9,92,5};
+//  
+liste. AddRange (sayılar);
+foreach (var item in liste)
+    Console.WriteLine(item);
+//Sort
+Console.WriteLine("***** Sort *****");
+liste. Sort();
+
+foreach (var item in liste)
+    Console.WriteLine(item);
+// Binary Search
+Console.WriteLine("***** Binary Search *****");
+Console.WriteLine(liste.BinarySearch(9));
+//Reverse
+Console.WriteLine("***** Reverse *****");
+liste. Reverse();
+foreach (var item in liste)
+    Console.WriteLine(item);
+//Clear
+Console.WriteLine("***** Clear *****");
+liste.Clear();
+foreach (var item in liste)
+    Console.WriteLine(item);
+
+
+
+}
+}
+}
